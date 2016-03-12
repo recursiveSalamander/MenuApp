3a149121c80aecb6e2e267c9b0d87724955c0617
bllnd <bllnd@mac.com>
2016-03-12 15:48:59 -0800

    [feature] implements restaurant name, address and phone number

--- client/restaurantList/restaurantListController.js
+++ client/restaurantList/restaurantListController.js
@@ -3,14 +3,17 @@ angular.module('menuApp')
 .controller('restaurantListController', function($window, $scope, menuAppFactory) {
   $scope.data = [];
 
-  $scope.searchRestaurants = function() {
+  $scope.displayRestaurants = function() {
     menuAppFactory.getRestaurantList()
     .then(function(data) {
       console.log('++line 9 inside restaurantListController searchRestaurants',data);
+      data.forEach(function(value) {
+        value.formatted = value.location.formattedAddress.join();
+      })
       $scope.data = data;
     }).catch(function(err) {
       console.log(err);
     })
   }
-  $scope.searchRestaurants();
+  $scope.displayRestaurants();
 });
