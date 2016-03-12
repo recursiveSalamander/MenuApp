1460e70f829377d0241884e4797a3201e83a421d
bllnd <bllnd@mac.com>
2016-03-12 14:57:18 -0800

    [feature] implements restaurant listing with dummy data

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,10 +3,8 @@ var menuController = require('../controllers/menuController.js');
 
 module.exports = function(app, express) {
 
-
-
   app.use('/', express.static(__dirname + '/../../client'));
 
   app.get('/api/restaurantList', restaurantController.getRestaurants);
-  app.get('/api/menu', menuController.getMenu);
+  // app.get('/api/menu', menuController.getMenu);
 };
