96a3a53cba5af9cacec307245e12dfcec9ca3313
Hyungki <hyungkip@gmail.com>
2016-03-12 12:25:20 -0800

    [bugfix] fixes rebase

--- server/server.js
+++ server/server.js
@@ -13,10 +13,6 @@ var server = app.listen(port, function() {
   console.log('http://localhost:' + port);
 });
 
-<<<<<<< 99d9923de79f7f47a10fd2ebd03dab131256a013
-module.exports = app;
-=======
 var io = require('socket.io')(server);
 
 module.exports = app;
->>>>>>> [setup] sets up socket io, sets up basic route structure, sets up dummy json in restaurantController, sets up front end methods for restaurant search
