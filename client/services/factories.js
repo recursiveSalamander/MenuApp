47fdd70eb5f37839ec27d88c1d4d8faed6bd25b8
Hyungki <hyungkip@gmail.com>
2016-03-12 12:16:12 -0800

    [setup] sets up socket io, sets up basic route structure, sets up dummy json in restaurantController, sets up front end methods for restaurant search

--- client/services/factories.js
+++ client/services/factories.js
@@ -2,8 +2,18 @@ angular.module('menuApp')
 
 .factory('menuAppFactory', function($http) {
 
+  var getRestaurantList = function (address) {
+    return $http({
+      method: 'GET',
+      url: '/api/restaurantList',
+      data: address
+    })
+    .then(function (resp) {
+      return resp.data;
+    })
+  }
 
   return {
-
+    getRestaurantList: getRestaurantList
   };
 });
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var request = require('request');
-
 var app = express();
 
 var db = require('./db/schema.js');
@@ -14,4 +13,10 @@ var server = app.listen(port, function() {
   console.log('http://localhost:' + port);
 });
 
+<<<<<<< 99d9923de79f7f47a10fd2ebd03dab131256a013
 module.exports = app;
+=======
+var io = require('socket.io')(server);
+
+module.exports = app;
+>>>>>>> [setup] sets up socket io, sets up basic route structure, sets up dummy json in restaurantController, sets up front end methods for restaurant search
