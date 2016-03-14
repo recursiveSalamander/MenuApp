0df1dd1e09358c59ac03359c02bda37565a0df86
Hyungki <hyungkip@gmail.com>
2016-03-14 15:47:28 -0700

    [features] setups up backbone for sign in and sign up pages, changes app.js appropriately

--- client/app.js
+++ client/app.js
@@ -4,12 +4,20 @@ angular.module('menuApp', [
 .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
   $stateProvider
   .state('restaurantList', {
-    url: '/restaurantlist',
+    url: '/restaurantList',
     templateUrl: '/restaurantList/restaurantList.html'
   })
   .state('menuView', {
-    url: '/menuview',
+    url: '/menuView',
     templateUrl: '/menuView/menuView.html'
+  })
+  .state('signIn', {
+    url: '/signIn',
+    templateUrl: '/signIn/signIn.html'
+  })
+  .state('signUp', {
+    url: '/signUp',
+    templateUrl: '/signUp/signUp.html'
   })
   $urlRouterProvider.otherwise('/restaurantList');
 });
---
+++ client/userAuth/userAuthController.js
@@ -0,0 +1,5 @@
+angular.module('menuApp')
+
+.controller('userAuthController', function($window, $scope) {
+
+});
