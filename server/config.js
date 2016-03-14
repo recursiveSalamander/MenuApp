8afa683eccfa0de3c0d6ebb7bc9aaee40e9d5fd1
andrewomacdonald <andrew.o.macdonald@gmail.com>
2016-03-14 15:31:55 -0700

    [Deploy] Created config vars for deployment

---
+++ server/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  foursquare: {
+    clientId: '5IR3RTVB1MTDPNONUDTNQKHU1WX2E1ZJLFFP5FEVGHDQ3JYI',
+    clientSecret: 'ZALSI20DU5WOQURHUJGGYATCDHHZIJEQ0HZC12AW3FMS2LQG'
+  },
+  database: {
+    host      : 'mysqlcluster8.registeredsite.com',
+    username  : 'hadywahby',
+    password  : '!Qaz2wsx',
+    database  : 'mksthesis',
+    charset   : 'utf8'
+  }
+};
