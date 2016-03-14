c29233ea218dcd72b23f2d9c829163d282be00e6
andrewomacdonald <andrew.o.macdonald@gmail.com>
2016-03-14 15:31:55 -0700

    [Deploy] Created config vars for deployment

--- server/controllers/restaurantController.js
+++ server/controllers/restaurantController.js
@@ -1,8 +1,14 @@
 var request = require('request');
 var _ = require('lodash');
 var moment = require('moment');
-
-var config = require('../db/config/config.js');
+if(!process.env.CLIENTID){
+  var config = require('../config.js');
+  var clientId = config.foursquare.clientId;
+  var clientSecret = config.foursquare.clientSecret;
+} else {
+  var clientId = process.env.CLIENTID;
+  var clientSecret = process.env.CLIENTSECRET;
+};
 
 module.exports = {
 
@@ -12,8 +18,8 @@ module.exports = {
     var date = moment().format('YYYYMMDD');
 
     var query = `https://api.foursquare.com/v2/venues/search?ll=${lat},${long}` +
-                `&limit=50&categoryId=4bf58dd8d48988d1c4941735&client_id=${config.foursquare.clientId}` +
-                `&client_secret=${config.foursquare.clientSecret}&v=${date}`;
+                `&limit=50&categoryId=4bf58dd8d48988d1c4941735&client_id=${clientId} ` +
+                `&client_secret=${clientSecret}&v=${date}`;
 
     request(query, function(err, resp, body) {
       if (!err && resp.statusCode === 200) {
