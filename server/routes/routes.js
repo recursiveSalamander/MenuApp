3bbebbd6e327f5e66b5fb809f28dd4a9b00a381e
bllnd <bllnd@mac.com>
2016-03-15 11:33:27 -0700

    [feature] implements logic for clicking on restaurantList to menu

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,6 +5,6 @@ module.exports = function(app, express) {
 
   app.use('/', express.static(__dirname + '/../../client'));
 
-  app.get('/api/restaurants' ,restaurantController.getRestaurants);
-  app.get('/api/menu' ,menuController.getMenu);
+  app.get('/api/restaurants', restaurantController.getRestaurants);
+  app.get('/api/menu', menuController.getMenu);
 };
