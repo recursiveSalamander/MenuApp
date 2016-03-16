4fe3030bfd80d5a10a132001cac40d450c49ad9b
andrewomacdonald <andrew.o.macdonald@gmail.com>
2016-03-16 10:31:08 -0700

    [Fix] Changed function name in controllers, working on login sessions

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -10,4 +10,5 @@ module.exports = function(app, express) {
   app.post('/api/restaurants', bodyParser, restaurantController.getRestaurants);
   app.get('/api/menu', bodyParser, menuController.getMenu);
   app.post('/api/users/signup', userAuthController.signup);
+  // app.post('api/users/signin', userAuthController.signin);
 };
