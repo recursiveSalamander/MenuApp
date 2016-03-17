66d072133997abfb177d355604fc7f1456fc7d04
andrewomacdonald <andrew.o.macdonald@gmail.com>
2016-03-16 21:04:30 -0700

    [Fix] Wrote table schema, working on models for them tomorrow

--- server/db/schema.js
+++ server/db/schema.js
@@ -33,42 +33,42 @@ db.knex.schema.hasTable('users').then(function(exists) {
   }
 });
 
-db.knex.schema.hasTable('item_ratings').then(function(exists) {
-  if(!exists){
-    knex.schema.createTable('item_ratings', function(rating) {
-      rating.increments('id').primary();
-      rating.integer('rating');
-      rating.integer('user_id');
-      rating.foreign('user_id').references('id').inTable('users');
-      rating.integer('item_id');
-      rating.foreign('item_id').references('id').inTable('menu_items');
-    }).then(function() {
-      console.log('item_ratings table has been created');
-    })
-  }
-});
-
-db.knex.schema.hasTable('menu_items').then(function(exists) {
-  if(!exists){
-    knex.schema.createTable('menu_items', function(menuitems) {
-      menuitems.increments('id').primary();
-      menuitems.integerå('restaurant_id');
-      menuitems.foreign('restaurant_id').references('id').inTable('restaurants');
-    }).then(function() {
-      console.log('menu_items table has been created');
-    })
-  }
-});
-
-db.knex.schema.hasTable('restaurants').then(function(exists) {
-  if(!exists){
-    knex.schema.createTable('restaurants', function(restaurant) {
-      restaurant.increments('id').primary();
-      restaurant.string('restaurant_id', 50);
-    }).then(function() {
-      console.log('restaurants table has been created');
-    })
-  }
-});
+// db.knex.schema.hasTable('item_ratings').then(function(exists) {
+//   if(!exists){
+//     knex.schema.createTable('item_ratings', function(rating) {
+//       rating.increments('id').primary();
+//       rating.integer('rating');
+//       rating.integer('user_id');
+//       rating.foreign('user_id').references('id').inTable('users');
+//       rating.integer('item_id');
+//       rating.foreign('item_id').references('id').inTable('menu_items');
+//     }).then(function() {
+//       console.log('item_ratings table has been created');
+//     })
+//   }
+// });
+//
+// db.knex.schema.hasTable('menu_items').then(function(exists) {
+//   if(!exists){
+//     knex.schema.createTable('menu_items', function(menuitems) {
+//       menuitems.increments('id').primary();
+//       menuitems.integerå('restaurant_id');
+//       menuitems.foreign('restaurant_id').references('id').inTable('restaurants');
+//     }).then(function() {
+//       console.log('menu_items table has been created');
+//     })
+//   }
+// });
+//
+// db.knex.schema.hasTable('restaurants').then(function(exists) {
+//   if(!exists){
+//     knex.schema.createTable('restaurants', function(restaurant) {
+//       restaurant.increments('id').primary();
+//       restaurant.string('restaurant_id', 50);
+//     }).then(function() {
+//       console.log('restaurants table has been created');
+//     })
+//   }
+// });
 
 module.exports.bookshelf = bookshelf;
