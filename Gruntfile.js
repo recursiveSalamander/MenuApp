850c86dac9d94f6f87dc7c4b2f7f8a09effa24f7
Hady Wahby <wahby.hady@gmail.com>
2016-03-16 19:30:54 -0700

    [dev] expands grunt configurations

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,12 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-purifycss');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-casperjs');
 
@@ -15,6 +17,17 @@ module.exports = function(grunt) {
       dist: 'dist/*'
     },
 
+    copy: {
+      client: {
+        src: [ 'client/index.html', 'client/**/*.html' ],
+        dest: 'dist/'
+      },
+      server: {
+        src: ['server/**'],
+        dest: 'dist/'
+      }
+    },
+
     jshint: {
       files: ['client/**/*.js', 'client/*.js', 'server/*'],
       options: {
@@ -23,10 +36,19 @@ module.exports = function(grunt) {
       }
     },
 
+    purifycss: {
+      options: {},
+      target: {
+        src: ['client/*.html', 'client/**/*.html'],
+        css: ['client/styles/styles.css'],
+        dest: 'dist/purestyles.css'
+      }
+    },
+
     cssmin: {
       build: {
         files: {
-          'dist/styles.min.css': ['client/styles/styles.css']
+          'dist/purestyles.min.css': ['dist/purestyles.css']
         }
       }
     },
@@ -49,11 +71,21 @@ module.exports = function(grunt) {
       }
     },
 
+    casperjs: {
+      options: {},
+      e2e: {
+        files: {
+          'results/casper':
+        }
+      }
+    },
+
     watch: {
       scripts: {
         files: ['client/*.js','client/**/*.js'],
         tasks: ['concat', 'uglify']
       },
+
       css: {
         files: 'client/styles/styles.css',
         tasks: ['cssmin']
