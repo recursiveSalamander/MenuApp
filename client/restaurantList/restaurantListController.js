e3e1060e78566cdb750c34a2a3df33a4677140ce
Hyungki <hyungkip@gmail.com>
2016-03-27 18:31:26 -0700

    [feature] adds markers for the restaurants displayed in the list

--- client/restaurantList/restaurantListController.js
+++ client/restaurantList/restaurantListController.js
@@ -2,38 +2,95 @@ angular.module('menuApp')
 
 .controller('restaurantListController', ['$window', '$scope', '$state', 'menuAppFactory', 'Geolocation', 'Auth', 'userInfo', function($window, $scope, $state, menuAppFactory, Geolocation, Auth, userInfo) {
   $scope.data = [];
+  $scope.map;
   $scope.checkToken = function(){
     return Auth.isAuth();
   }
 
+  var markers = [];
+  var makeMap = function(current_coords) {
+    $scope.map = new google.maps.Map(document.getElementById('map'), {
+      center: current_coords,
+      scrollwheel: false,
+      zoom: 14
+    });
+    markers.push(new google.maps.Marker({
+      position: current_coords,
+      map: $scope.map,
+      title: 'hello'
+    }));
+
+    };
+
+  var addMarkerWithTimeout = function (position) {
+
+        markers.push(new google.maps.Marker({
+          position: position,
+          map: $scope.map,
+          animation: google.maps.Animation.DROP
+        }));
+
+  }
+
+  var clearMarkers = function (callback) {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+    markers = [];
+  }
+
+
+  var refocusMapBounds = function () {
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < markers.length; i++) {
+     bounds.extend(markers[i].getPosition());
+    }
+
+    $scope.map.fitBounds(bounds);
+  }
+  // data.forEach(function(restaurant) {
+  //   restaurant.formatted = restaurant.location.formattedAddress.join();
+  //   var LatLng = {lat: restaurant.location.lat, lng: restaurant.location.lng};
+  //
+  //   var infoWindow = new google.maps.InfoWindow({
+  //     content: restaurant.name
+  //   })
+  //   addMarkerWithTimeout(LatLng, 0);
+  //   console.log(markers);
+  //
+  //   // markers[].addListener('click', function(){
+  //   //   infoWindow.open($scope.map, marker);
+  //   // })
+  // });
+
+
+
+
 
   $scope.displayRestaurants = function() {
+    clearMarkers();
     Geolocation.getLatLong(function(lat, lng) {
       Geolocation.formatLatLong(lat, lng, function(coords) {
         menuAppFactory.getRestaurantList(coords)
         .then(function(data) {
-          console.log(data);
-          data.forEach(function(value) {
-            value.formatted = value.location.formattedAddress.join();
-            var LatLng = {lat: value.location.lat, lng: value.location.lng};
-            var infoWindow = new google.maps.InfoWindow({
-              content: value.name
-            })
-            var marker = new google.maps.Marker({
-              position: LatLng,
-              map: $scope.map,
-              title: 'restaurant'
-            });
-            marker.addListener('click', function(){
-              infoWindow.open($scope.map, marker);
-            })
-          });
+          console.log('djfoidjfiosjdoifsajoifjiosadfo',data);
+
+          for(var i=0; i< data.length; i++){
+            data[i].formatted = data[i].location.formattedAddress.join();
+            var LatLng = {lat: data[i].location.lat, lng: data[i].location.lng};
+            addMarkerWithTimeout(LatLng);
+            //SETTIMEOUT HELP PLS
+          }
           $scope.data = data;
+          refocusMapBounds();
+          //FIX THIS TOO
+          console.log('hhriereirheirererere',markers)
         })
         .catch(function(err) {
           console.log(err);
         });
       });
+
     });
   };
 
@@ -77,6 +134,7 @@ angular.module('menuApp')
 
   autocomplete.addListener('place_changed', function() {
     var place = autocomplete.getPlace();
+    console.log('place', place)
     if(!place.geometry) {
       window.alert("Autocomplete's returned place contains no geometry");
       return;
@@ -90,7 +148,11 @@ $scope.map.setZoom(17);  // Why 17? Because it looks good.
 }
 
 $scope.displayRestaurants();
+
+
 });
+
+
 
 function initMap() {
 // Create a map object and specify the DOM element for display.
@@ -108,18 +170,7 @@ if (navigator.geolocation) {
     makeMap(current_coords);
   });
 
-  var makeMap = function(current_coords) {
-    $scope.map = new google.maps.Map(document.getElementById('map'), {
-      center: current_coords,
-      scrollwheel: false,
-      zoom: 14
-    });
-    $scope.marker = new google.maps.Marker({
-      position: current_coords,
-      map: $scope.map,
-      title: 'hello'
-      })
-    };
+
 
   }
 }
