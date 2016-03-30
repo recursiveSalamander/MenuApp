e14d9ae33587c3766d0ccfca295a2b8173e4f31d
bllnd <bllnd@mac.com>
2016-03-29 19:46:29 -0700

    [feature] implements loading animation

--- client/preferenceForm/preferenceFormController.js
+++ client/preferenceForm/preferenceFormController.js
@@ -99,10 +99,10 @@ angular.module('menuApp')
       preferredIngredients: $scope.preferredIngredients,
       rejectedIngredients: $scope.rejectedIngredients
     };
-    console.log('++line 115 pre postUserPreference in preferenceFormController preferenceData: ',preferenceData);
+    // console.log('++line 115 pre postUserPreference in preferenceFormController preferenceData: ',preferenceData);
     menuAppFactory.postUserPreference(preferenceData)
     .then(function(data) {
-      console.log('++line 117 post postUserPreference in preferenceFormController data: ',data);
+      // console.log('++line 117 post postUserPreference in preferenceFormController data: ',data);
       // $state.go('menuView', {menuData: data});
     });
 
