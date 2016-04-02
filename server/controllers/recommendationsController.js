174411454b2935068141ae4ca21a854edb1350d3
Hady Wahby <wahby.hady@gmail.com>
2016-04-02 10:58:05 -0700

    [refactor] partial modularization of yummly request code

--- server/controllers/recommendationsController.js
+++ server/controllers/recommendationsController.js
@@ -1,12 +1,25 @@
 var request = require('request');
-var _= require('lodash');
+var _ = require('lodash');
+var Async = require('async');
+var Bluebird = require('bluebird');
 var User = require('../db/models/User.js');
 var Utils = require('../utils.js');
-var Promise = require('bluebird');
 var User_Preference = require('../db/models/User_Preference.js');
-var Async = require('async');
+var config = require('../db/config/config.js');
+
 
 module.exports = {
+  restrictionsRecommendationRewrite: function(req, res) {
+    var token = req.body.currentToken;
+    var userID = Utils.getUserID(token);
+    var menu = req.body.menuData;
+
+    getUserRestrictionInfo(userID, function(diet, allergies) {
+      injectDietRestrictions()
+    });
+
+  },
+
   restrictionsRecommendation: function(req, res) {
     var menuData = req.body.menuData;
     var response_Ultimate = res;
@@ -17,7 +30,7 @@ module.exports = {
 
 
     var getUserRestrictionInfo = function(callback) {
-      console.log('inside getUserRestrictionInfo')
+      console.log('inside getUserRestrictionInfo');
       var token = req.body.currentToken;
       var userID = Utils.getUserID(token);
       User.where({id: userID}).fetchAll({withRelated: ['preferences']})
@@ -199,7 +212,7 @@ module.exports = {
 
             })(x);
           }
-          setTimeout(callback2, 5000);
+          setTimeout(callback2(), 5000);
     }
 
     var sendRestrictionData = function(){
@@ -216,3 +229,184 @@ module.exports = {
 
 }
 }
+
+var injectDietRestrictions = function(menu, diet, allergies, callback) {
+  var counter = 0;
+
+  _.forEach(menu, function(submenu, index) {
+    _.forEach(submenu, function(entry, subindex) {
+      var query = generateQuery(entry);
+
+      queryRecipes(query, function() {})
+    })
+  });
+}
+
+var generateQuery = function(item) {
+  var searchTerms = item.name.split(' ');
+  var searchquery = searchTerms.join('+');
+
+  var query = `http://api.yummly.com/v1/api/recipes?_app_id=${config.yummly.appId}` +
+  `&_app_key=${config.yummly.appKey}&q=${searchquery}&facetField[]=diet`;
+
+  return query;
+}
+
+
+var queryRecipes = function(query, callback) {
+  request(query, function(err, resp, body) {
+    var allergyRestriction = {mostlikely: [], may: []};
+    var dietRestriction = {};
+
+    if (!err && resp.statusCode === 200) {
+      var data = JSON.parse(body);
+      var totalRecipeNumber = data.totalMatchCount;
+
+      if (totalRecipeNumber >= 5) {
+        _.forEach(data.facetCounts.diet, function(key) {
+          var trimmedDietKey = diet.slice(4);
+          var trimmedAllergyKey = diet.slice(4, -5);
+          var percentage = data.facetCounts.diet[key] / totalRecipeNumber;
+
+          if (key === diet) {
+            if(percentage < 0.66 && percentage >= 0.33) {
+              dietRestriction.diet = trimmedDietKey;
+              dietRestriction.message = 'possibly';
+            }
+            if(percentage < 0.33) {
+              dietRestriction.diet = trimmedDietKey;
+              dietRestriction.message = 'most likely';
+            }
+          }
+
+          _.forEach(allergies, function(allergy) {
+            if (key === allergy) {
+              if(percentage < 0.66 && percentage >= 0.33) {
+                allergyRestriction.may.push(trimmedAllergyKey);
+              }
+              if(percentage < 0.33) {
+                allergyRestriction.mostlikely.push(trimmedAllergyKey);
+              }
+            }
+          })
+        });
+      }
+      callback(dietRestriction, allergyRestriction);
+    }
+  });
+};
+
+
+var yummlyAPIcalls = function(menu, diet, allergies, callback){
+  var counter = 0;
+
+  for(var x=0; x< menuData.length; x++){
+    (function(x){
+      for(var y=0; y < menuData[x].entries.items.length; y++){
+        (function(y){
+          console.log('start of inside for loop------------------------------------------------------')
+          var key_words = menuData[x].entries.items[y].name.split(' ');
+          console.log('KREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE_words', key_words);
+          var searchquery = key_words.join('+');
+          var query = `http://api.yummly.com/v1/api/recipes?_app_id=0a1658f1&_app_key=26fcc7b19149942523604cf763d9321d&q=` + searchquery +
+          `&facetField[]=diet`;
+
+          //variables diet, disliked, liked, and allergies are ready for processing
+
+          request(query, function(err, resp, body) {
+            console.log('REQUEST START FOR KEYOWRD', searchquery)
+            var allergyRestriction = {mostlikely: [], may: []};
+            var dietRestriction = {};
+            if (!err && resp.statusCode === 200) {
+              var data = JSON.parse(body);
+              var totalRecipeNumber = data.totalMatchCount;
+
+              if(totalRecipeNumber >= 5){
+                for(var key in data.facetCounts.diet){
+                  var trimmedDietKey = diet.slice(4);
+                  var percentage = data.facetCounts.diet[diet] / totalRecipeNumber;
+
+                  if(key === diet){
+                    if(percentage < 0.66 && percentage >= 0.33){
+                      dietRestriction.diet = trimmedKey;
+                      dietRestriction.message = 'possibly';
+                    }
+                    if(percentage < 0.33){
+                      dietRestriction.diet = trimmedKey;
+                      dietRestriction.message = 'most likely';
+                    }
+                  }
+                }
+
+                for(var key in data.facetCounts.diet){
+                  var trimmedKey = key.slice(4, -5);
+                  var percentage = data.facetCounts.diet[key] / totalRecipeNumber;
+
+                  for(var i=0; i<allergies.length; i++){
+                    if(allergies[i] === key){
+
+                      if(percentage < 0.66 && percentage >= 0.33){
+                        allergyRestriction.may.push(trimmedKey);
+                      }
+                      if(percentage < 0.33){
+                        allergyRestriction.mostlikely.push(trimmedKey);
+                      }
+                    }
+                  }
+                }
+
+              }
+              else {
+                console.log('AHHHHHHHHHHHHHHHHHHHHHHHHHHHH TOTAL RECIPE NUMBER IS LESS THAN 5')
+              }
+            }
+            menuData[x].entries.items[y].allergyRestriction = allergyRestriction;
+            menuData[x].entries.items[y].dietRestriction = dietRestriction;
+            console.log('++line 166 in recommendationsController menudata: ',menuData[x].entries.items[y]);
+            console.log('allergyRestriction', allergyRestriction);
+            console.log('dietRestriction', dietRestriction);
+          });
+
+        })(y);
+      }
+    })(x);
+  }
+}
+
+var getUserRestrictionInfo = function(userID, callback) {
+  User.where({id: userID}).fetchAll({withRelated: ['preferences']})
+  .then(function(data){
+    var data = data.toJSON()[0];
+    var diet = dietCodes[data.diet];
+    var allergies = [];
+
+    _.forEach(data.preferences, function(preference) {
+      if (preference.relation === 'allergy') {
+        allergies.push(allergyCodes[preference.ingredient])
+      }
+    });
+    callback(diet, allergies);
+  });
+};
+
+var allergyCodes = {
+  'Wheat'   : '392^Wheat-Free',
+  'Gluten'  : '393^Gluten-Free',
+  'Peanut'  : '394^Peanut-Free',
+  'Tree nut': '395^Tree Nut-Free',
+  'Dairy'   : '396^Dairy-Free',
+  'Egg'     : '397^Egg-Free',
+  'Seafood' : '398^Seafood-Free',
+  'Sesame'  : '399^Sesame-Free',
+  'Soy'     : '400^Soy-Free',
+  'Sulfite' : '401^Sulfite-Free'
+};
+
+var dietCodes = {
+  'vegan'            : '386^Vegan',
+  'vegetarian'       : '387^Lacto-ovo vegetarian',
+  'lacto vegetarian' : '388^Lacto vegetarian',
+  'ovo vegetarian'   : '389^Ovo vegetarian',
+  'pescetarian'      : '390^Pescetarian',
+  'paleo'            : '403^Paleo'
+};
